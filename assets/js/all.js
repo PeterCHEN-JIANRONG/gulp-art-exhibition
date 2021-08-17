9054ad00cf809fbd1dea1b68e7102da3476d00dc
peter.chen1024 <u0052017@gmail.com>
2021-08-17 22:58:12 +0800

    Update 2021-08-17T14:57:57.591Z

---
+++ assets/js/all.js
@@ -0,0 +1,22 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30 // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true
+  // }
+
+});
+var swiper = new Swiper(".mySwiper-md", {
+  slidesPerView: 2.7,
+  spaceBetween: 30 // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true
+  // }
+
+});
+//# sourceMappingURL=all.js.map
