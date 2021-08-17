2dc3c2f2edfa05813fcb1f17fce183622a6ee706
peter.chen1024 <u0052017@gmail.com>
2021-08-17 22:54:07 +0800

    add index/猜你也喜歡 樣式

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,21 @@
-$(function() {
+$(function () {
   console.log('Hello Bootstrap5');
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true
+  // }
+});
+
+var swiper = new Swiper(".mySwiper-md", {
+  slidesPerView: 2.7,
+  spaceBetween: 30,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true
+  // }
 });
