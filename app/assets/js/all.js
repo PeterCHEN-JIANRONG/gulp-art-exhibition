bfaa6b6914aa38166067327995b6f01be4f552c8
peter.chen1024 <u0052017@gmail.com>
2021-08-24 22:21:20 +0800

    update swiper結構調整

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -5,15 +5,20 @@ $(function () {
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
-  // pagination: {
-  //   el: ".swiper-pagination",
-  //   clickable: true
-  // }
-});
-
-var swiper = new Swiper(".mySwiper-md", {
-  slidesPerView: 2.7,
-  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    767: {
+      slidesPerView: 1.7,
+    },
+    992: {
+      slidesPerView: 2.7,
+      spaceBetween: 30,
+    }
+  },
   // pagination: {
   //   el: ".swiper-pagination",
   //   clickable: true
